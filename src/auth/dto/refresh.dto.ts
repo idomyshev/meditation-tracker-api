a12d4b25f29ad4ceb6c705c09af4079cdeb546b3
idomyshev <iadomyshev@gmail.com>
2025-07-28 18:45:40 +0200

    fix: refresh token is empty

--- src/auth/dto/refresh.dto.ts
+++ src/auth/dto/refresh.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RefreshDto {
   @ApiProperty({
     description: 'Refresh token for getting new access token',
     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
   })
+  @IsString()
+  @IsNotEmpty()
   refresh_token: string;
 }
